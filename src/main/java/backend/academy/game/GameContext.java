fd362f107623dcf44620d03d1e261af9e126824b
Anna Shrubok <annashrubok21@gmail.com>
2024-09-11 15:49:51 +0300

    feat: add getters to GameContext private fields

--- src/main/java/backend/academy/game/GameContext.java
+++ src/main/java/backend/academy/game/GameContext.java
@@ -2,22 +2,41 @@ package backend.academy.game;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class GameContext {
 
     @SuppressWarnings("membername")
-    final int MAX_MISTAKES;
-    int mistakes = 0;
-    final String answer;
-    char[] currentAnswer;
-    Set<Character> processedLetters = new HashSet<>();
+    private final int MAX_MISTAKES;
+    private int mistakes = 0;
+
+    private final String answer;
+    private final char[] currentAnswer;
+    private final Set<Character> processedLetters = new HashSet<>();
+
+
+    public String getAnswer() {
+        return answer;
+    }
 
     public GameContext(int maxMistakes, String word) {
         MAX_MISTAKES = maxMistakes;
         answer = word;
         currentAnswer = new char[word.length()];
         Arrays.fill(currentAnswer, '_');
+    }
+
+    public char[] getCurrentAnswer() {
+        return Arrays.copyOf(currentAnswer, currentAnswer.length);
+    }
+
+    public List<Character> getProcessedLetters() {
+        return List.copyOf(processedLetters);
+    }
+
+    public int getRemainingAttempts() {
+        return MAX_MISTAKES-mistakes;
     }
 
     public boolean processLetter(char letter) {
