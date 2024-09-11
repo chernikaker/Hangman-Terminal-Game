468ad75121c94666b9ca7ae6f6e89d4891641688
Anna Shrubok <annashrubok21@gmail.com>
2024-09-11 21:09:45 +0300

    test: add tests for context constructor

--- src/test/java/backend/academy/game/GameContextTest.java
+++ src/test/java/backend/academy/game/GameContextTest.java
@@ -5,19 +5,45 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GameContextTest {
 
     @Nested
-    class constructorTests {
+    class ConstructorTests {
 
-        
+        @Test
+        void validArgs() {
+            String word = "hello";
+            int mistakes = 10;
+            assertDoesNotThrow(()->new GameContext(mistakes, word));
+        }
+
+        @Test
+        void invalidWord(){
+            String word = "";
+            int mistakes = 10;
+            assertThatThrownBy(()->new GameContext(mistakes,word))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Word cannot be empty");
+        }
+
+        @Test
+        void invalidMistakesAmount(){
+            String word = "hello";
+            int mistakes = 0;
+            assertThatThrownBy(()->new GameContext(mistakes,word))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Amount of mistakes must be at least 1");
+        }
+
     }
 
     @Nested
-    class processLetterTests {
+    class ProcessLetterTests {
 
         @ParameterizedTest
         @CsvSource({"b, b_____","B, b_____","a, _a_a_a","A, _a_a_a"})
