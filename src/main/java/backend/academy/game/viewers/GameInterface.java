80734998175e538bfd69bcbb8eb546c232a49827
Anna Shrubok <annashrubok21@gmail.com>
2024-09-20 22:43:20 +0300

    feat: add jline console cleaning
    Jline dependency and maven shade plugin were added in pom.xml.

--- src/main/java/backend/academy/game/viewers/GameInterface.java
+++ src/main/java/backend/academy/game/viewers/GameInterface.java
@@ -1,6 +1,9 @@
 package backend.academy.game.viewers;
 
+import backend.academy.game.Dictionary;
 import backend.academy.game.GameContext;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
@@ -18,8 +21,22 @@ public abstract class GameInterface {
         scanner = new Scanner(in);
     }
 
+    public String getInput() {
+        return scanner.nextLine();
+    }
+
+    public void clearScreen(){
+        try {
+            Terminal terminal = TerminalBuilder.terminal();
+            terminal.puts(org.jline.utils.InfoCmp.Capability.clear_screen);
+            terminal.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public abstract void viewScreen(GameContext context);
-    public abstract String getInput();
-    public abstract GameInterface changeInterface();
+    public abstract GameInterface changeInterface(boolean validated, boolean processed);
     public abstract void viewStartScreen();
+    public abstract void viewSettingsScreen(Dictionary dictionary);
 }
