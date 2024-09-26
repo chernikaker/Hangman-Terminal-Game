53fa5783007b5d61169847bace89d9c59d15e3a9
Anna Shrubok <annashrubok21@gmail.com>
2024-09-27 00:06:44 +0300

    feat(UI): improve screens design

--- src/main/java/backend/academy/game/visualizers/PlayerInterface.java
+++ src/main/java/backend/academy/game/visualizers/PlayerInterface.java
@@ -9,14 +9,15 @@ import java.util.Scanner;
 import lombok.extern.slf4j.Slf4j;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
+import static backend.academy.game.visualizers.HangmanVisualizer.hangmanSteps;
 
 @Slf4j
 @SuppressWarnings("magicnumber")
 public class PlayerInterface {
 
     private final PrintStream out;
-
     private final Scanner scanner;
+
     private final HangmanVisualizer visualizer = new HangmanVisualizer();
     private final Terminal terminal;
 
@@ -24,12 +25,11 @@ public class PlayerInterface {
     public PlayerInterface(PrintStream out, InputStream in) {
         this.out = out;
         scanner = new Scanner(in, StandardCharsets.UTF_8);
-
         try {
             terminal = TerminalBuilder.terminal();
         }
         catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Terminal can't be created: " + e);
         }
     }
 
@@ -59,14 +59,16 @@ public class PlayerInterface {
     public void viewSettingCategoryScreen(List<String> categories, boolean previousInvalidInput) {
         clearScreen();
         out.println("""
-             █▀▀ █ █ █▀█ █▀█ █▀▀ █▀▀   █▀▀ █▀█ ▀█▀ █▀▀ █▀▀ █▀█ █▀▄ █ █
-             █   █▀█ █ █ █ █ ▀▀█ █▀▀   █   █▀█  █  █▀▀ █ █ █ █ █▀▄  █
-             ▀▀▀ ▀ ▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀   ▀▀▀ ▀ ▀  ▀  ▀▀▀ ▀▀▀ ▀▀▀ ▀ ▀  ▀
+            ╔═══════════════════════════════════════════════════════════╗
+            ║ █▀▀ █ █ █▀█ █▀█ █▀▀ █▀▀   █▀▀ █▀█ ▀█▀ █▀▀ █▀▀ █▀█ █▀▄ █ █ ║
+            ║ █   █▀█ █ █ █ █ ▀▀█ █▀▀   █   █▀█  █  █▀▀ █ █ █ █ █▀▄  █  ║
+            ║ ▀▀▀ ▀ ▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀   ▀▀▀ ▀ ▀  ▀  ▀▀▀ ▀▀▀ ▀▀▀ ▀ ▀  ▀  ║
+            ╚═══════════════════════════════════════════════════════════╝
             """);
 
         int categoryColumns = 3;
         for (int i = 0; i < categories.size(); i++) {
-            out.printf("%-25s", categories.get(i));
+            out.printf("%-25s", categories.get(i).toUpperCase());
             if ((i + 1) % categoryColumns == 0) {
                 out.println();
             }
@@ -74,25 +76,28 @@ public class PlayerInterface {
         if (categories.size() % categoryColumns != 0) {
             out.println();
         }
+        out.println("═══════════════════════════════════════════════════════════");
+        out.println();
         if (previousInvalidInput) {
-            out.println("Error: no such category");
+            out.println("Error: no such category. Try again!");
         }
-        out.print("Enter one of the categories(leave empty to choose random): ");
-
+        out.print("Enter one of the categories [leave empty to choose random]: ");
     }
 
     public void viewSettingDifficultyScreen(List<Integer> difficulties, boolean previousInvalidInput) {
         clearScreen();
         out.println("""
-              █▀▀ █ █ █▀█ █▀█ █▀▀ █▀▀   █▀▄ ▀█▀ █▀▀ █▀▀ ▀█▀ █▀▀ █ █ █   ▀█▀ █ █
-              █   █▀█ █ █ █ █ ▀▀█ █▀▀   █ █  █  █▀▀ █▀▀  █  █   █ █ █    █   █
-              ▀▀▀ ▀ ▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀   ▀▀  ▀▀▀ ▀   ▀   ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀  ▀   ▀
-              █   █▀▀ █ █ █▀▀ █
-              █   █▀▀ ▀▄▀ █▀▀ █
-              ▀▀▀ ▀▀▀  ▀  ▀▀▀ ▀▀▀
+            ╔═══════════════════════════════════════════════════════════════════╗
+            ║ █▀▀ █ █ █▀█ █▀█ █▀▀ █▀▀   █▀▄ ▀█▀ █▀▀ █▀▀ ▀█▀ █▀▀ █ █ █   ▀█▀ █ █ ║
+            ║ █   █▀█ █ █ █ █ ▀▀█ █▀▀   █ █  █  █▀▀ █▀▀  █  █   █ █ █    █   █  ║
+            ║ ▀▀▀ ▀ ▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀   ▀▀  ▀▀▀ ▀   ▀   ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀  ▀   ▀  ║
+            ║                       █   █▀▀ █ █ █▀▀ █                           ║
+            ║                       █   █▀▀ ▀▄▀ █▀▀ █                           ║
+            ║                       ▀▀▀ ▀▀▀  ▀  ▀▀▀ ▀▀▀                         ║
+            ╚═══════════════════════════════════════════════════════════════════╝
             """);
 
-        int difficultyColumns = 2;
+        int difficultyColumns = 5;
         for (int i = 0; i < difficulties.size(); i++) {
             out.printf("%-15s", difficulties.get(i));
             if ((i + 1) % difficultyColumns == 0) {
@@ -102,10 +107,13 @@ public class PlayerInterface {
         if (difficulties.size() % difficultyColumns != 0) {
             out.println();
         }
+
+        out.println("═════════════════════════════════════════════════════════════════════");
+        out.println();
         if (previousInvalidInput) {
             out.println("Error: no such difficulty level");
         }
-        out.print("Enter one of the levels(leave empty to choose random): ");
+        out.print("Enter one of the levels [leave empty to choose random]: ");
 
     }
 
@@ -113,20 +121,25 @@ public class PlayerInterface {
     public void viewHangmanScreen(GameContext context, boolean previousInvalidInput) {
         clearScreen();
         drawHangman(context);
-        if (previousInvalidInput) {
-            out.println("Error: invalid input, enter one letter, which was not checked");
-        }
+
         out.print("Checked letters: ");
         for (char c : context.getProcessedLetters()) {
             out.print(c + " ");
         }
         out.println();
+
         out.println("Remaining attempts: " + (context.maxMistakes() - context.mistakes()));
+        out.println();
+
         out.println("CURRENT ANSWER");
         for (char c : context.getCurrentAnswer()) {
             out.print(c);
         }
         out.println();
+        out.println("————————————————————");
+        if (previousInvalidInput) {
+            out.println("Error: invalid input, enter one letter, which was not checked. Try again!");
+        }
         out.print("Enter next letter: ");
     }
 
@@ -134,22 +147,28 @@ public class PlayerInterface {
         clearScreen();
         if (context.mistakes() == context.maxMistakes()) {
             drawHangman(context);
-            out.println("You lost! Answer was:" + context.answer());
+            out.println("YOU LOST!\nAnswer was: " + context.answer());
         } else {
             out.println("""
-               \\O/
-                |
-               / \\""");
-            out.println("You won! Answer: " + context.answer());
+                ██╗    ██╗██╗███╗   ██╗
+                ██║    ██║██║████╗  ██║
+                ██║ █╗ ██║██║██╔██╗ ██║
+                ██║███╗██║██║██║╚██╗██║
+                ╚███╔███╔╝██║██║ ╚████║
+                 ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝
+                   \\O/ \\O/ \\O/ \\O/
+                    |   |   |   |
+                   / \\ / \\ / \\ / \\""");
+            out.println("\nYOU WON!\nAnswer: " + context.answer());
         }
     }
 
     public void drawHangman(GameContext context) {
         out.println("█▀▀▀▀▀█");
-        int ropeCount = context.maxMistakes() - context.mistakes() > 6
-            ? context.mistakes() : context.maxMistakes() - 6;
-        int hangmanCount = context.maxMistakes() - context.mistakes() > 6
-            ? 0 : 6 - context.maxMistakes() + context.mistakes();
+        int ropeCount = context.maxMistakes() - context.mistakes() > hangmanSteps
+            ? context.mistakes() : context.maxMistakes() - hangmanSteps;
+        int hangmanCount = context.maxMistakes() - context.mistakes() > hangmanSteps
+            ? 0 : hangmanSteps - context.maxMistakes() + context.mistakes();
         for (int i = 0; i < ropeCount; i++) {
             out.println("█     |");
         }
@@ -158,7 +177,7 @@ public class PlayerInterface {
         for (int i = 0; i < remainingHeight+1; i++) {
             out.println("█");
         }
-        out.println("----------");
+        out.println("═════════════");
     }
 
     public void clearScreen() {
